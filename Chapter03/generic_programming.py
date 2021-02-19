ed46310e8f673f6ac5a26942e88b45db8b20e515
Yeseong31 <66625672+yeseong31@users.noreply.github.com>
2021-02-19 14:21:26 +0900

    Add files via upload

--- Chapter03/generic_programming.py
+++ Chapter03/generic_programming.py
@@ -3,7 +3,7 @@
 # def are_equal(a, b):
 #   return a == b
   
-# are_equal(10, 10.0)
+# print(are_equal(10, 10.0))
  
 # 파이썬은 원래 동적 타이핑 언어이므로 제네릭이 필요 없지만
 # 타입을 아예 명시하지 않으면 가독성을 낮추고 버그 발생 확률이 높아진다.
@@ -17,7 +17,7 @@ U = TypeVar('U')
 def are_equal(a: T, b: U) -> bool:
     return a == b
  
-are_equal(10, 10.0)
+print(are_equal(10, 10.0))
  
 # PEP 484에 추가된 타입 힌트를 통해
 # 이처럼 제네릭을 사용할 수 있다.(단, 3.5+에서만)
